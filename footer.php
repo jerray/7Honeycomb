edea17ebd42dad46abad51b07c9443a6b3ec0cda
7lemon <7lemonsoul@gmail.com>
2011-03-10 10:42:14 +0800

    add pureweber link

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <div id="footer">
 	<div class="copyright">
-		<p>Copyright &#169; <a href="<?php bloginfo( 'url' );?>"><?php bloginfo( 'name' );?></a> 2011 <a href="http://wordpress.org" target="_blank">Powered by Wordpress</a> &#124; <a href="http://www.7lemon.net">Theme designed by 7lemon</a></p>
+		<p>Copyright &#169; <a href="<?php bloginfo( 'url' );?>"><?php bloginfo( 'name' );?></a> <?php echo date('Y');?> Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a></p>
+		<p>Theme designed by <a href="http://www.7lemon.net" target="_blank">7lemon</a> Of <a href="http://www.pureweber.com" target="_blank">Pureweber</a></p>
 	</div>
 	<div class="backtotop"><p><a href="#wrapper" id="return_top">回到顶部</a></p></div>
 </div><!--footer-->
